52c829a61c2f4c78e4d04e6cfdf27e1fa97edad3
Pedro Victor <pedro.victor.ferreira@outlook.com.br>
2016-12-06 16:16:36 -0400

    IA do inimigo
    - Campo de visão
    - Movimentação por Waypoints

---
+++ TheBurglar/Assets/Scripts/Path/PathScript.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+
+public class PathScript : MonoBehaviour {
+
+    public Color rayColor = Color.white;
+    public Color selectedColor = Color.blue;
+    public List<Transform> pathObjs = new List<Transform>();
+    Transform[] theArray;
+    public float sphereSize = 0.5f;
+
+    public bool isLoop;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = rayColor;
+        theArray = GetComponentsInChildren<Transform>();
+        pathObjs.Clear();
+        foreach(Transform path_Obj in theArray){
+            if(path_Obj != this.transform){
+                pathObjs.Add(path_Obj);
+            }
+        }
+
+        for(int i = 0; i < pathObjs.Count; i++){
+            Vector3 position = pathObjs[i].position;
+            if (i > 0){
+                Vector3 previous = pathObjs[i - 1].position;
+                if (Selection.activeObject.Equals(pathObjs[i].gameObject)){
+                    Gizmos.color = selectedColor;
+                    Gizmos.DrawLine(previous, position);
+                    Gizmos.DrawWireSphere(position, sphereSize);
+                }else{
+                    Gizmos.color = rayColor;
+                    Gizmos.DrawLine(previous, position);
+                    Gizmos.DrawWireSphere(position, sphereSize);
+                }
+            }else{
+                Vector3 previous = pathObjs[1].position;
+                if (Selection.activeObject.Equals(pathObjs[i].gameObject)){
+                    Gizmos.color = selectedColor;
+                    Gizmos.DrawLine(previous, position);
+                    Gizmos.DrawWireSphere(position, sphereSize);
+                }else{
+                    Gizmos.color = rayColor;
+                    Gizmos.DrawLine(previous, position);
+                    Gizmos.DrawWireSphere(position, sphereSize);
+                }
+                if (isLoop){
+                    previous = pathObjs[pathObjs.Count-1].position;
+                    Gizmos.DrawLine(previous, position);
+                }
+            }
+
+        }
+
+    }
+
+}
