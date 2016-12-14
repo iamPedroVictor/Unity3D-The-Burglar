df0fd3ed3ca789f79bb40fd71013feff41c45799
Emiliandro <Emiliandro@live.com>
2016-12-14 16:49:34 -0400

    saidas

--- TheBurglar/Assets/Scripts/IA/Objectives/ObjectsList.cs
+++ TheBurglar/Assets/Scripts/IA/Objectives/ObjectsList.cs
@@ -6,7 +6,8 @@ public class ObjectsList : MonoBehaviour {
     public GameObject[] objectsTargets;
     private TargetObject target;
     public bool canExit = false;
-
+    public GameObject saida;
+    bool semsaida = false;
     // Use this for initialization
     void Start () {
         RandomObject();
@@ -15,10 +16,15 @@ public class ObjectsList : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (CheckQuest()){
-            //Faça alguma coisa
+        if (semsaida) {
+            if (CheckQuest()) ChamarSaida();
         }
 	}
+
+    void ChamarSaida() {
+        Instantiate(saida);
+        semsaida = true;
+    }
 
     private void RandomObject(){
         if (objectsTargets.Length == 0)
---
+++ TheBurglar/Assets/Scripts/NextLevelLoad.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NextLevelLoad : MonoBehaviour {
+
+    public string proximo;
+
+    void OnTriggerEnter(Collider other) {
+        if (other.tag == "Player") {
+            Application.LoadLevel(proximo);
+        }
+    }
+
+
+}
