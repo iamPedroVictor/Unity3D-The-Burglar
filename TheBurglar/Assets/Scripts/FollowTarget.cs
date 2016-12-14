662cd0391dfae41faa438f6a9b343bb7209f397d
Emiliandro <Emiliandro@live.com>
2016-12-14 16:06:52 -0400

    camera fix

---
+++ TheBurglar/Assets/Scripts/FollowTarget.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowTarget : MonoBehaviour {
+
+    [Header("Qual o alvo?")]
+    public GameObject alvo;
+    [Header("Qual a velocidade da perseguição")]
+    public float velocidade;
+
+	void FixedUpdate () {
+        if (transform.position != alvo.transform.position)
+            transform.position = Vector3.MoveTowards(transform.position, alvo.transform.position, velocidade * Time.deltaTime);
+    }
+}
