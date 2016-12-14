f33c4888bd265322ea866772a1ebcff63ac31d01
Pedro Victor <pedro.victor.ferreira@outlook.com.br>
2016-12-14 00:53:26 -0400

    Player Moviment

---
+++ TheBurglar/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField]
+    private float movimentSpeed = 5f;
+    [SerializeField]
+    private float rotateSpeed = 30f;
+
+    // Component caching
+    private PlayerMotor motor;
+
+    // Use this for initialization
+    void Start () {
+        motor = GetComponent<PlayerMotor>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float _xMov = Input.GetAxis("Horizontal");
+        float _zMov = Input.GetAxis("Vertical");
+
+        float _Moviment = (_zMov * movimentSpeed) * Time.deltaTime;
+        float _Rotation = rotateSpeed * _xMov * Time.deltaTime;
+        Vector3 _Rotate = new Vector3(0, _Rotation, 0);
+        Debug.Log("Moviment: " + _Moviment);
+        Debug.Log("Rotate: " + _Rotate);
+        Vector3 _velocity = new Vector3(0, 0, _Moviment);
+
+        Debug.Log("Velocity: " + _velocity);
+        //Apply movement
+        motor.Move(_velocity);
+        motor.Rotate(_Rotate);
+
+
+    }
+}
---
+++ TheBurglar/Assets/Scripts/Player/PlayerMotor.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class PlayerMotor : MonoBehaviour {
+
+    private Vector3 velocity = Vector3.zero;
+    private Vector3 rotation = Vector3.zero;
+
+    private Transform tf;
+
+    private void Start(){
+        tf = GetComponent<Transform>();
+    }
+
+    // Gets a movement vector
+    public void Move(Vector3 _velocity){
+        velocity = _velocity;
+    }
+
+    // Gets a rotational vector
+    public void Rotate(Vector3 _rotation){
+        rotation = _rotation;
+    }
+
+    void PerformMovement(){
+        if(velocity != Vector3.zero){
+            tf.Translate(velocity);
+        }
+    }
+
+    void PerformRotation(){
+        tf.Rotate(rotation);
+    }
+
+    private void Update(){
+        PerformMovement();
+        PerformRotation();
+    }
+
+}
